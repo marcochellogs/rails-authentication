f39898694a1198d62eda0e67f78e4c8abb92732a
marco checchi <marco.checchi2@gmail.com>
2023-03-06 10:21:17 +0200

    created repo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+  # before_action :authenticate_user! => from application.controller
+  skip_before_action :authenticate_user!, only: :home
+
   def home
   end
+  def about
+  end
+
 end
